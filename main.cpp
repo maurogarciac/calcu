f6a2df69c0d1aa8f5dd05887c3212d95b34bb7ef
Mauro Garcia Coto <maurogarciacoto@gmail.com>
2023-08-30 23:40:21 +0300

    i wrote code that doesn't compile

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <string>
+
+int main(){
+
+	int num1, num2;
+	string ehhh;
+	getline(ehhh);
+	cout<< "test:" << ehhh << endl;
+	return 0;
+}
